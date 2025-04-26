3d3c8a22197129e2a7f4e70269f2e3abf58dad92
RamsesYok <ramses-studie@memoad.jp>
2025-04-26 11:35:10 +0900

    メイン処理と設定ファイルを追加

---
+++ rosterly/src/main/java/com/example/rosterly/WebapiApplication.java
@@ -0,0 +1,11 @@
+package com.example.rosterly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebapiApplication.class, args);
+    }
+}
